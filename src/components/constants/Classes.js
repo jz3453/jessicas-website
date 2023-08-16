92b7db125fd4133a75a7b5ef9ed55101d404494d
jz3453 <jz3453@columbia.edu>
2023-08-15 19:08:47 -0500

    added courses back

---
+++ src/components/constants/Classes.js
@@ -0,0 +1,132 @@
+export const semesters = [
+  {
+      semester: 'Sophomore Spring (Spring 2023)',
+      courses: [
+          {
+              'course-name': 'Intro to Databases',
+              'course-number': 'COMS W4111',
+              'professor': 'Donald Ferguson',
+          },
+          {
+              'course-name': 'Fundamentals of Computer Systems',
+              'course-number': 'CSEE W3827',
+              'professor': 'Daniel Rubenstein',
+          },
+          {
+              'course-name': 'Computer Science Theory',
+              'course-number': 'COMS W3261',
+              'professor': 'Xi Chen',
+          },
+          {
+              'course-name': 'Projects in Computer Science',
+              'course-number': 'COMS BC3997',
+              'professor': 'Brian Plancher',
+          },
+          {
+              'course-name': 'Applied Statistical Computing',
+              'course-number': 'STAT UN2102',
+              'professor': 'Alex Pijyan',
+          },
+          {
+              'course-name': 'The Science of Psychology',
+              'course-number': 'PSYC UN1001',
+              'professor': 'Patricia Lindemann',
+          },
+      ],
+  },
+  {
+      semester: 'Sophomore Fall (Fall 2022)',
+      courses: [
+          {
+              'course-name': 'Advanced Programming',
+              'course-number': 'COMS W3157',
+              'professor': 'John Hui',
+          },
+          {
+              'course-name': 'Computational Linear Algebra',
+              'course-number': 'COMS W3251',
+              'professor': 'Daniel Hsu',
+          },
+          {
+              'course-name': 'Intro to Statistics',
+              'course-number': 'STAT UN1101',
+              'professor': 'Alex Pijyan',
+          },
+          {
+              'course-name': 'Principles of Economics',
+              'course-number': 'ECON UN1105',
+              'professor': 'Prajit Dutta',
+          },
+          {
+              'course-name': 'Masterpieces of Western Art',
+              'course-number': 'HUMA UN1121',
+              'professor': 'Sarah Cohen',
+          },
+      ],
+  },
+  {
+      semester: 'Freshman Spring (Spring 2022)',
+      courses: [
+          {
+              'course-name': 'Honors Data Structures and Algorithms',
+              'course-number': 'COMS W3137',
+              'professor': 'Daniel Bauer',
+          },
+          {
+              'course-name': 'Discrete Math',
+              'course-number': 'COMS W3203',
+              'professor': 'Ansaf Salleb-Aouissi',
+          },
+          {
+              'course-name': 'Intro to Electricity and Magnetism',
+              'course-number': 'PHYS UN1402',
+              'professor': 'Georgia Karagiorgi',
+          },
+          {
+              'course-name': 'General Chemistry Laboratory',
+              'course-number': 'CHEM UN1500',
+              'professor': 'Joseph Ulichny',
+          },
+          {
+              'course-name': 'University Writing: Readings in Data and Society',
+              'course-number': 'ENGL CC1010',
+              'professor': 'Julia Ryan',
+          },
+      ],
+  },
+  {
+      semester: 'Freshman Fall (Fall 2021)',
+      courses: [
+          {
+              'course-name': 'Intro to Computer Science and Programming in Java',
+              'course-number': 'COMS W1004',
+              'professor': 'Paul Blaer',
+          },
+          {
+              'course-name': 'Intro to Computing for Engineers and Applied Scientists',
+              'course-number': 'ENGI E1006',
+              'professor': 'Daniel Bauer',
+          },
+          {
+              'course-name': 'The Art of Engineering',
+              'course-number': 'ENGI E1102',
+              'professor': 'David Vallancourt',
+          },
+          {
+              'course-name': 'Multivariable Calculus for Engineers',
+              'course-number': 'APMA E2000',
+              'professor': 'Drew Youngren',
+          },
+          {
+              'course-name': 'Intro to Mechanics and Thermodynamics',
+              'course-number': 'PHYS UN1401',
+              'professor': 'Tanya Zelevinsky',
+          },
+          {
+              'course-name': 'General Chemistry I',
+              'course-number': 'CHEM UN1403',
+              'professor': 'Wei Min',
+          },
+      ],
+  },
+]
--- src/components/routes/courses/Courses.js
+++ src/components/routes/courses/Courses.js
@@ -1,11 +1,70 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { DisplayContext } from '../../../App';
+import { Accordion, AccordionSummary, AccordionDetails, IconButton } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './Courses.css';
-
+import '../../general.css';
+import { semesters } from '../../constants/Classes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Courses() {
+  const { narrowDisplay, setNarrowDisplay } = useContext(DisplayContext);
+
   return (
-    <div>Courses</div>
+    <div className={narrowDisplay ? 'page-full': 'page'}>
+      <div className='page-contents'>
+        <div className='title'>
+          Coursework
+        </div>
+        <div className='courses-container'>
+        {
+          semesters.map((semester) => {
+            return (
+              <div className='semester-section' key={semester.semester}>
+                <Accordion className='course-list'>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                  >
+                    <div className='semester-title'>{semester.semester}</div>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className='honors'>
+                      Honors: Dean's List
+                      <FontAwesomeIcon icon="fa-solid fa-star" size="sm" />
+                    </div>
+                    <div className='course-list'>
+                      {
+                        semester.courses.map((course) => {
+                          return (
+                            <div className='course' key={course['course-name']}>
+                              <div className='course-name'>
+                                {course['course-name']}
+                              </div>
+                              <div className='course-description'>
+                                <div className='course-subtext highlight'>
+                                  {course['course-number']}
+                                </div>
+                                <div className='course-subtext'>
+                                  {course['professor']}
+                                </div>
+                              </div>
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            )
+          })
+        }
+        <div className='source-container'>
+          <div className='source-text'>Built using MUI Accordion.</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default Courses;
